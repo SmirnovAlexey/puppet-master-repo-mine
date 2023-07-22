96c09a2049d28fb18639fee609eaaa09a8844ad0
SmirnovAlexey <alexey.work@tut.by>
2023-07-22 14:19:49 +0300

    Update inst_nginx.pp

--- manifests/inst_nginx.pp
+++ manifests/inst_nginx.pp
@@ -1,12 +1,3 @@
 class inst_nginx {
   include nginx
-
-  file { 'nginx config':
-    owner     => 'root',
-    group     => 'root',
-    mode      => '0644',
-    ensure    => file,
-    path      => '/etc/nginx/nginx.conf',
-    source    => '/vagrant/master/nginx.conf',
-  }
 }
