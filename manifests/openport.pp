e04e36c8236e2d7bfbdcd1bba55f82a3f558bc53
SmirnovAlexey <alexey.work@tut.by>
2023-07-22 16:18:32 +0300

    Update openport.pp

--- manifests/openport.pp
+++ manifests/openport.pp
@@ -1,17 +1,7 @@
-class openports {
+class openport {
     class { 'firewall': }
     firewall { '100 open tcp port 80':
       port => 80,
-      proto => 'tcp',
-      action => 'accept',
-    }
-    firewall { '100 open tcp port 8081':
-      port => 8081,
-      proto => 'tcp',
-      action => 'accept',
-    }
-    firewall { '100 open tcp port 8082':
-      port => 8082,
       proto => 'tcp',
       action => 'accept',
     }
