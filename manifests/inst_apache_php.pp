9bb67cc1a3c32c8795bf756d0cf7a9d23685e6eb
SmirnovAlexey <alexey.work@tut.by>
2023-07-22 18:28:08 +0300

    Create inst_apache_php.pp

---
+++ manifests/inst_apache_php.pp
@@ -0,0 +1,13 @@
+class inst_apache_php {
+  include apache
+  class { 'apache::mod::php':
+    php_version => '7.4'
+  }
+
+  file { 'index.php':
+   ensure => file,
+   path   => "/var/www/html/index.php",
+   source => "/vagrant/slave/index.php"
+   notify => Service['httpd']
+ }
+}
