f81c30938ed3669d1dc3ab88c659bc769e605dfd
SmirnovAlexey <alexey.work@tut.by>
2023-07-22 17:48:10 +0300

    Update inst_nginx.pp

--- manifests/inst_nginx.pp
+++ manifests/inst_nginx.pp
@@ -8,8 +8,10 @@ class inst_nginx {
     ensure    => file,
     path      => '/etc/nginx/conf.d/default.conf',
     source    => '/vagrant/master/nginx.conf',
+    notify    => Service['nginx'],
   } 
-  ~> service {
+  
+  service {
     'nginx':
         ensure => true,
         enable => true,
