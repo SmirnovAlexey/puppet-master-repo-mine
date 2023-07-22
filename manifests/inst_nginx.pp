b0ec1d36e2ed190abfb9857cb2ba99d2ff203f00
SmirnovAlexey <alexey.work@tut.by>
2023-07-22 16:05:48 +0300

    Update inst_nginx.pp

--- manifests/inst_nginx.pp
+++ manifests/inst_nginx.pp
@@ -2,12 +2,12 @@ class inst_nginx {
   include nginx
   
   nginx::resource::server { 'static':
-    listen_port => 81,
+    listen_port => 8081,
     proxy       => 'http://192.168.33.11',
   } 
   
   nginx::resource::server { 'dynamic':
-    listen_port => 82,
+    listen_port => 8082,
     proxy       => 'http://192.168.33.12',
   } 
   
