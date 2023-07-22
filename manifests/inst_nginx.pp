2b8de35ad436ff75d714b545ad0807d3269ed40a
SmirnovAlexey <alexey.work@tut.by>
2023-07-22 14:49:36 +0300

    Update inst_nginx.pp

--- manifests/inst_nginx.pp
+++ manifests/inst_nginx.pp
@@ -1,11 +1,13 @@
 class inst_nginx {
   include nginx
   nginx::resource::location { "static":
+    server         => 'localhost',
     ensure         => present,
     location       => '/static',
     fastcgi        => "192.168.33.10",
   } 
   nginx::resource::location { "dynamic":
+    server         => 'localhost',
     ensure         => present,
     location       => '/dynamic',
     fastcgi        => "192.168.33.11",
