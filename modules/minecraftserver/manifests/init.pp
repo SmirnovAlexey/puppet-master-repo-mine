c0450cbf3047470c64c821e33e3b4f391c26119c
SmirnovAlexey <alexey.work@tut.by>
2023-07-23 10:58:52 +0300

    Create init.pp

---
+++ modules/minecraftserver/manifests/init.pp
@@ -0,0 +1,26 @@
+class minecraftserver {
+
+  file { '/opt/minecraft':
+    ensure => directory,
+    before => File['minecraft'],
+  }
+
+  file { 'minecraft':
+    path => "/opt/minecraft/server.jar",
+    source => "https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar",
+    mode => "755",
+    require => File['/opt/minecraft'],
+  }
+
+  file { 'minecraft_service':
+    path => "/etc/systemd/system/minecraftserver.service",
+    source => "puppet:///modules/minecraft/minecraftserver.service",
+    notify => Service['minecraft'],
+  }
+
+  service { 'minecraft':
+    ensure => running,
+    require => File['minecraft_service'],
+  }
+
+}
