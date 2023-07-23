abc0d6ac6ad332122fb7c57115f17698bfec0d81
SmirnovAlexey <alexey.work@tut.by>
2023-07-23 21:38:37 +0300

    Update init.pp

--- modules/minecraftserver/manifests/init.pp
+++ modules/minecraftserver/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraftserver {
   package { 'java 17':
     ensure => 'installed',
     source => 'https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm',
-    name   => 'jdk-17_linux-x64_bin.rpm'
+    name   => 'jdk-17_linux-x64_bin.rpm',
     provider => 'rpm'
   }
   
