91a7f95556469c3a562d19eb95cfb532cf5d3c3b
SmirnovAlexey <alexey.work@tut.by>
2023-07-24 16:01:08 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,13 @@
 node 'master.puppet' {
   include inst_nginx
-  #include openport
 }
 
 node 'slave1.puppet' {
   include inst_apache
-  #include openport
 }
 
 node 'slave2.puppet' {
   include inst_apache_php
-  #include openport
 }
 
 node 'mineserver.puppet' {
